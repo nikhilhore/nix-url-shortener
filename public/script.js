472d7442370235addc13bdcbe41ea30449d370c8
nikhilhore <niknsh5@gmail.com>
2022-03-05 01:37:07 +0530

    Added copy button

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
-const submitBtn = document.querySelector('#submitBtn');
 const longInput = document.querySelector('#longUrl');
 const shortInput = document.querySelector('#shortUrl');
+const submitBtn = document.querySelector('#submitBtn');
+const copyBtn = document.querySelector('#copyBtn');
 
 async function handleSubmit() {
     const longUrl = longInput.value;
@@ -10,4 +11,14 @@ async function handleSubmit() {
     shortInput.value = shortUrl;
 }
 
+function copyToClipboard() {
+    const shortUrl = shortInput.value;
+    navigator.clipboard.writeText(shortUrl);
+    copyBtn.innerText = 'Copied!';
+    setTimeout(() => {
+        copyBtn.innerText = 'Copy';
+    }, 2000);
+}
+
 submitBtn.addEventListener('click', handleSubmit);
+copyBtn.addEventListener('click', copyToClipboard);
