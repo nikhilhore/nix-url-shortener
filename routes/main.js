52c80faeb67c00302d482da7113be67db646614e
nikhilhore <niknsh5@gmail.com>
2022-03-05 00:28:15 +0530

    added some try catch blocks

--- routes/main.js
+++ routes/main.js
@@ -7,32 +7,44 @@ const LongUrlAndShortId = require('./../models/LongUrlAndShortId');
 const site = "https://nix-us.herokuapp.com/";
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
+    try {
+        res.sendFile(path.join(__dirname, '..', 'index.html'));
+    } catch (e) {
+        res.send('There was some error while accessing this site');
+    }
 });
 
 router.post('/convertLongUrlToShort', async (req, res) => {
-    const { longUrl } = req.body;
-    let shortUrl = site;
-    const result = await LongUrlAndShortId.findOne({ longUrl }).exec();
-    if (result == null) {
-        const shortId = nanoid(15);
-        shortUrl += shortId;
-        new LongUrlAndShortId({ longUrl, shortId }).save();
+    try {
+        const { longUrl } = req.body;
+        let shortUrl = site;
+        const result = await LongUrlAndShortId.findOne({ longUrl }).exec();
+        if (result == null) {
+            const shortId = nanoid(15);
+            shortUrl += shortId;
+            new LongUrlAndShortId({ longUrl, shortId }).save();
+        }
+        else {
+            shortUrl += result.shortId;
+        }
+        res.json({ shortUrl });
+    } catch (err) {
+        res.json({ shortUrl: "", err })
     }
-    else {
-        shortUrl += result.shortId;
-    }
-    res.json({ shortUrl });
 });
 
 router.get('/:shortId', async (req, res) => {
-    const shortId = req.params.shortId;
-    const result = await LongUrlAndShortId.findOne({ shortId }).exec();
-    let longUrl = site;
-    if (result != null) {
-        longUrl = result.longUrl;
+    try {
+        const shortId = req.params.shortId;
+        const result = await LongUrlAndShortId.findOne({ shortId }).exec();
+        let longUrl = site;
+        if (result != null) {
+            longUrl = result.longUrl;
+        }
+        res.redirect(longUrl);
+    } catch (err) {
+        res.json({ shortUrl: "", err })
     }
-    res.redirect(longUrl);
 });
 
 module.exports = router;
